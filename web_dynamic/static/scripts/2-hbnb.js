e086804a885e02e941a00ea25bee31806c9b031c
fabo893 <jose.rosa893@gmail.com>
2021-05-17 00:47:07 +0000

    2 javaScript

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,24 @@
+amenityDict = {}
+$("checkbox").click(function () {
+  $(this).each(function () {
+    if (this.checked) {
+      amenityDict[$(this).attr("data-id")] = $(this).attr("data-name");
+    } else {
+      delete amenityDict[$(this).attr("data-id")];
+    }
+  });
+  if (amenityDict.values()) {
+    $("div.amenities h4").text(amenity.values().join(', '));
+  }
+});
+
+
+$(function () {
+  $.get("http://0.0.0.0:5001/api/v1/status/", function(data, textStatus) {
+    if (textStatus === "OK") {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+});
