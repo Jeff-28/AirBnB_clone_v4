0b51df8cdf22b782f8d86d59e56a95bde3a640c8
Jeff Martinez <2408@holbertonschool.com>
2021-06-03 17:57:33 +0000

    new file

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -12,10 +12,9 @@ $("checkbox").click(function () {
   }
 });
 
-
 $(function () {
-  $.get("http://0.0.0.0:5001/api/v1/status/", function(data, textStatus) {
-    if (textStatus === "OK") {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+    if (data.status === 'OK') {
       $('DIV#api_status').addClass('available');
     } else {
       $('DIV#api_status').removeClass('available');
